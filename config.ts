51a1a67b17546ed9e81c457faffdb18364ec3d16
3lang <675483520@qq.com>
2023-07-03 11:55:25 +0800

    chore: readme

--- config.ts
+++ config.ts
@@ -2,9 +2,9 @@ export const config = {
   /** 最高运行气价 */
   MAX_GAS_PRICE: 5000,
   /** 加价抢跑 */
-  ADD_GAS_PRICE: 20,
+  ADD_GAS_PRICE: 200,
   /** 是否强制执行到成功 */
-  loopUtilSuccess: true,
+  loopUtilSuccess: false,
   /** 账号多的建议换成收费节点的rpc，公用有速率和流量限制 */
-  rpc: 'https://linea-testnet.rpc.thirdweb.com'
+  rpc: 'https://linea-goerli.infura.io/v3/be1401a29cbf405cad0a9192d0e40418'
 }
